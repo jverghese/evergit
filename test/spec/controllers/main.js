45f3a6ac7e1e237a66812628c10e68e62a9afe68
jverghese <johann.verghese@gmail.com>
2014-02-16 22:30:57 -0800

    Add get notes api endpoint and change content of index page.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -12,17 +12,17 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/awesomeThings')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+    $httpBackend.expectGET('/api/notes')
+      .respond([{ text: 'note 1'}, { text: 'note 2'}]);
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings).toBeUndefined();
+  it('should attach a list of notes to the scope', function () {
+    expect(scope.notes).toBeUndefined();
     $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
+    expect(scope.notes.length).toBe(2);
   });
 });
